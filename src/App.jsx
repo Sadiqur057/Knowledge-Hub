a8f67052f9f19cc9bb82ec4162997799467b3b07
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-14 10:55:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-green-300 text-2xl font-bold'>Knowledge Hub</h1>
+      <div>
+       
+      </div>
+      
+    </>
+  )
+}
+
+export default App
